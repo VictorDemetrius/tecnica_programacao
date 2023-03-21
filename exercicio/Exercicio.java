b5857b9f437f96a7658459e9857fab5a70895921
Victor Demetrius Marqueto <73915190+VictorDemetrius@users.noreply.github.com>
2023-03-21 10:27:38 -0300

    Add files via upload

---
+++ exercicio/Exercicio.java
@@ -0,0 +1,123 @@
+package exercicio;
+// @author Victor D. Marqueto
+import javax.swing.JOptionPane;
+public class Exercicio {
+
+    public static void main(String[] args) {
+        
+       // Exercicio 1
+        
+        /*int mes;
+        
+        mes = Integer.parseInt(JOptionPane.showInputDialog("Digite o mês do seu aniversário: "));
+        
+        switch (mes){
+            
+            case 1 : 
+                JOptionPane.showMessageDialog(null,"Seu aniversário é em Janeiro");
+            break;
+            
+            case 2 : 
+                JOptionPane.showMessageDialog(null,"Seu aniversário é em Fevereiro");
+            break;
+            
+            case 3 : 
+                JOptionPane.showMessageDialog(null,"Seu aniversário é em Março");
+            break;
+            
+            case 4 : 
+                JOptionPane.showMessageDialog(null,"Seu aniversário é em Abril");
+            break;
+            
+            case 5 : 
+                JOptionPane.showMessageDialog(null,"Seu aniversário é em Maio");
+            break;
+            
+            case 6 : 
+                JOptionPane.showMessageDialog(null,"Seu aniversário é em Junho");
+            break;
+            
+            case 7 : 
+                JOptionPane.showMessageDialog(null,"Seu aniversário é em Julho");
+            break;
+            
+            case 8 : 
+                JOptionPane.showMessageDialog(null,"Seu aniversário é em Agosto");
+            break;
+            
+            case 9 : 
+                JOptionPane.showMessageDialog(null,"Seu aniversário é em Setembro");
+            break;
+            
+            case 10 : 
+                JOptionPane.showMessageDialog(null,"Seu aniversário é em Outubro");
+            break;
+            
+            case 11 : 
+                JOptionPane.showMessageDialog(null,"Seu aniversário é em Novembro");
+            break;
+            
+            case 12 : 
+                JOptionPane.showMessageDialog(null,"Seu aniversário é em Dezembro");
+            break;
+            
+            default:
+                JOptionPane.showMessageDialog(null,"Mês inválido tente novamente");
+        
+        }*/
+        
+        
+       //Exercicio 2
+        
+       /* float n1,n2,n3;
+        
+        n1 = Float.parseFloat(JOptionPane.showInputDialog("Digite o primeiro número: "));
+        n2 = Float.parseFloat(JOptionPane.showInputDialog("Digite o primeiro número: "));
+        n3 = Float.parseFloat(JOptionPane.showInputDialog("Digite o primeiro número: "));
+
+       if(n1 > n2+n3)
+       {
+           JOptionPane.showMessageDialog(null,"O Primeiro número é maior que a soma dos dois últimos");
+       }
+       else
+           JOptionPane.showMessageDialog(null,"O Primeiro número não é maior que a soma dos dois últimos");
+       
+       
+       */
+       
+       
+       
+      // Exercicio 3 
+       
+       /*int velocidade,maxVelocidade, multa;
+       
+       
+       velocidade = Integer.parseInt(JOptionPane.showInputDialog("Digite a velocidade que o carro estava: "));
+       maxVelocidade = Integer.parseInt(JOptionPane.showInputDialog("Digite a velocidade máxima da via: "));
+       
+       multa = velocidade - maxVelocidade;
+       
+       if( multa <=0)
+            {
+                JOptionPane.showMessageDialog(null,"Velocidade dentro do limite da via ");
+            
+            }
+       else if(multa <=10)
+            {
+                JOptionPane.showMessageDialog(null,"Sua multa é de R$ 50,00 reais ");
+            
+            }
+       else if(multa <=30)
+           {
+               JOptionPane.showMessageDialog(null,"Sua multa é de R$ 100,00 reais ");
+               
+           }
+       else
+            {
+       
+               JOptionPane.showMessageDialog(null,"Sua multa é de R$ 300,00 reais ");
+            }
+     */
+}
+
+}
